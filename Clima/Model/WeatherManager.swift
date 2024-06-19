6e19ac8810653d264a1149452523e8426f582a31
Dmitry <mrsahadov@gmail.com>
2024-06-19 13:57:33 -0300

    Add all structs for the JSON decoder

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -43,7 +43,7 @@ struct WeatherManager {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.name)
+            print(decodedData.weather[0].description)
         } catch {
             print(error)
         }
