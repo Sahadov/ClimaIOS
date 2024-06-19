aba7cb859d5db22ef32cbb51473cd6c0f6f9b909
Dmitry <mrsahadov@gmail.com>
2024-06-19 13:45:03 -0300

    Add JSONparser function with a basic struct

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Дмитрий Волков on 19.06.2024.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+}
